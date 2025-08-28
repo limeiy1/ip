5f7b374597fcd221d2400b5b6434341340f10312
meiyi <e1385500@u.nus.edu>
2025-08-28 20:53:29 +0800

    Rename

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I'm\n" + logo);
+ //         String logo = " ____        _        \n"
+
+        System.out.println("Hello I'm mimi");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
