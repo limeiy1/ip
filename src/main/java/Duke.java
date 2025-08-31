4fb3b88f671eef1eaa35be52c8ba2fcf05db3d30
meiyi <e1385500@u.nus.edu>
2025-08-31 22:48:25 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,28 @@
-public class Duke {
-    public static void main(String[] args) {
- //         String logo = " ____        _        \n"
+import java.util.Scanner;
 
-        System.out.println("Hello I'm mimi");
+public class Duke {
+    private static final String LINE =
+            "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello I'm mimi!");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        do {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                return;
+            }
+            System.out.println(LINE);
+            System.out.println(line);
+            System.out.println(LINE);
+        } while (in.hasNextLine());
     }
 }
