d9e8b9aadbc7a66f19812eff713ddcf7081d28dd
meiyi <e1385500@u.nus.edu>
2025-09-03 01:03:29 +0800

    update Duke to include mark as done function
    added ability to mark and unmark the tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String line;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
         do {
             line = in.nextLine();
@@ -22,21 +22,36 @@ public class Duke {
                 System.out.println(LINE);
                 return;
             }
-            if (line.equalsIgnoreCase("list")) {
+            if (line.equalsIgnoreCase("list")) { // show list
                 System.out.println(LINE);
                 if (count == 0) {
                     System.out.println("List is empty!");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < count; i++) {
-                        System.out.println( (i+1) + ". " + list[i]);
+                        System.out.println((i + 1) + ". " + list[i].toString());
                     }
                 }
                 System.out.println(LINE);
-            } else {
+            } else if (line.startsWith("mark")) { // mark as done
+                int taskIndex = Integer.parseInt(line.split(" ")[1]) - 1;
+                list[taskIndex].markAsDone();
+                System.out.println(LINE);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list[taskIndex].toString());
+                System.out.println(LINE);
+            } else if (line.startsWith("unmark")) { // unmark as done
+                int taskIndex = Integer.parseInt(line.split(" ")[1]) - 1;
+                list[taskIndex].unmarkAsDone();
+                System.out.println(LINE);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(list[taskIndex].toString());
+                System.out.println(LINE);
+            } else { // add new task
                 System.out.println(LINE);
                 System.out.println("added: " + line);
                 System.out.println(LINE);
-                list[count] = line;
+                list[count] = new Task(line);
                 count++;
             }
         } while (in.hasNextLine());
