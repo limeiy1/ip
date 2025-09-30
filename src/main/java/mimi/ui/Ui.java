6b0596be9561225fd467876f37b29309fc9ac558
meiyi <e1385500@u.nus.edu>
2025-09-30 09:45:58 +0800

    add code that deals with interactions with the user

--- src/main/java/mimi/ui/Ui.java
+++ src/main/java/mimi/ui/Ui.java
@@ -1,5 +1,6 @@
-package mimi;
+package mimi.ui;
 
+import mimi.TaskList;
 import mimi.tasks.Task;
 
 import java.util.Scanner;
@@ -14,81 +15,62 @@ public class Ui {
         this.scanner = new Scanner(System.in);
     }
 
-    public void showWelcome() {
-        showLine();
-        System.out.println("Hello! I'm Mimi");
-        System.out.println("What can I do for you?");
-        showLine();
-    }
-
-    public static void showBye() {
-        showLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        showLine();
-    }
-
-    public static void showLine() {
-        System.out.println(LINE);
-    }
-
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    public void showLine() {
+        System.out.println(LINE);
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("Hello! I'm Mimi.");
+        System.out.println("What can I do for you?");
+        showLine();
+    }
+
+    public void showBye() {
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+
     public void showLoadingError() {
-        showLine();
         System.out.println("Error loading file. Starting with an empty task list.");
-        showLine();
     }
 
     public void showError(String message) {
-        showLine();
         System.out.println(message);
-        showLine();
     }
 
-    public static void showTaskList(TaskList tasks) {
-        showLine();
+    public void showTaskList(TaskList tasks) {
         if (tasks.size() == 0) {
-            System.out.println("Your task list is empty.");
+            System.out.println("Your task list is empty. Start adding to your list now!");
         } else {
-//            for (int i = 0; i < tasks.size(); i++) {
-//                System.out.printf("%d.%s%n", i + 1, tasks.get(i));
-//            }
             for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i + 1) + ". " + tasks.get(i).toString());
+                System.out.println((i + 1) + ". " + tasks.get(i));
             }
         }
-        showLine();
     }
 
-    public static void showAddedTask(Task task, int total) {
-        showLine();
+    public void showAddedTask(Task task, int total) {
         System.out.println("I've added this task:");
-        System.out.println(task.toString());
+        System.out.println(task);
         System.out.printf("Now you have %d task%s in the list.%n", total, total == 1 ? "" : "s");
-        showLine();
     }
 
-    public static void showDeletedTask(Task task, int total) {
-        showLine();
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(task.toString());
+    public void showDeletedTask(Task task, int total) {
+        System.out.println("I've removed this task:");
+        System.out.println(task);
         System.out.printf("Now you have %d task%s in the list.%n", total, total == 1 ? "" : "s");
-        showLine();
     }
 
-    public static void showMark(Task taskName) {
-        showLine();
-        System.out.println("Nice! I've marked this task as done:");
+    public void showMark(Task taskName) {
+        System.out.println("Yay! I've marked this task as done:");
         System.out.println(taskName);
-        showLine();
     }
 
-    public static void showUnmark(Task taskName) {
-        showLine();
-        System.out.println("OK, I've marked this task as not done yet:");
+    public void showUnmark(Task taskName) {
+        System.out.println("Aw ok, I've marked this task as not done yet:");
         System.out.println(taskName);
-        showLine();
     }
 }
